e81b4e6565ec44240addd4ac8f3a5bb9d97e2802
Fernando Martin Rey <reyfer17@gmail.com>
2022-07-19 17:56:48 -0300

    modificacion de formato de imagenes

--- script.js
+++ script.js
@@ -8,13 +8,15 @@ class Producto {
     }
 }
 
-const pochocloGrande = new Producto (1, "Pochoclo Grande", 1000, "dulce, en balde", 'images/pochocloGrande.webP')
+//const pochocloGrande = new Producto (1, "Pochoclo Grande", 1000, "dulce, en balde", 'images/pochocloGrande.webP')
+const pochocloGrande = new Producto (1, "Pochoclo Grande", 1000, "dulce, en balde", 'images/pochocloGrande.jpg')
 const pochocloMediano = new Producto (2, "Pochoclo Mediano", 800, "dulce, en caja", 'images/pochocloMediano.jpg')
 const pochocloChico = new Producto (3, "Pochoclo Chico", 600, "dulce, en bolsita", 'images/pochocloChico.gif')
 const gaseosaGrande = new Producto (4, "Gaseosa Grande", 500, "vaso x 950ml", 'images/gaseosaGrande.jpg')
 const gaseosaChica = new Producto (5, "Gaseosa Chica", 400, "botella x 500ml", 'images/gaseosaChica.jpg')
 const agua = new Producto (6, "Agua", 300, "botella x 500ml", 'images/agua.png')
-const chocolate = new Producto (7, "Chocolate", 800, "con leche x 300g", 'images/chocolate.webP')
+//const chocolate = new Producto (7, "Chocolate", 800, "con leche x 300g", 'images/chocolate.webP')
+const chocolate = new Producto (7, "Chocolate", 800, "con leche x 300g", 'images/chocolate.jpg')
 
 const productos = [pochocloGrande, pochocloMediano, pochocloChico, gaseosaGrande, gaseosaChica, agua, chocolate]
 
